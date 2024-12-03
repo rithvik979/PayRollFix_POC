3fa3abe9c0cb9a3b20bad6e619a064eee617a3ef
rithvik909 <121147728+rithvik909@users.noreply.github.com>
2024-12-03 14:54:27 +0530

    Update Program.cs

--- Payrollfix_poc/Program.cs
+++ Payrollfix_poc/Program.cs
@@ -78,7 +78,7 @@ public class program
         app.MapHub<NotificationHub>("/notificationHub"); // Make sure this matches client-side URL
 
         // Configure the HTTP request pipeline.
-        if (!app.Environment.IsProduction())
+        if (!app.Environment.IsDevelopment())
         {
             app.UseExceptionHandler("/Home/Error");
 			// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
