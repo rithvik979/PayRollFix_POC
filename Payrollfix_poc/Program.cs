338b7fe091d38f382920054bd5628086a812b02a
g.rithvik <g.rithvik909@gmail.com>
2024-12-11 12:22:07 +0530

    Development

--- Payrollfix_poc/Program.cs
+++ Payrollfix_poc/Program.cs
@@ -87,7 +87,7 @@ public class program
 		//{
 		//	// Prod-specific logic
 		//}
-		if (!app.Environment.IsProduction())
+		if (!app.Environment.IsDevelopment())
         {
             app.UseExceptionHandler("/Home/Error");
 			// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
